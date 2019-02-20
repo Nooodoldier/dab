2b5d221807495c66812bf3bad1761303c0a76325
hxp <15810665479@163.com>
2019-02-20 20:48:06 +0800

    新增springboot整合thymeleaf

--- src/main/java/com/bling/dab/controller/LoginController.java
+++ src/main/java/com/bling/dab/controller/LoginController.java
@@ -1,7 +1,9 @@
 package com.bling.dab.controller;
 
+import com.bling.dab.domain.User;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 import springfox.documentation.annotations.ApiIgnore;
 
 /**
@@ -9,12 +11,18 @@ import springfox.documentation.annotations.ApiIgnore;
  * @date: 2019/2/18 10:56
  * @description:
  */
-@RestController
 @ApiIgnore
+@RestController
 public class LoginController {
 
-    @RequestMapping(value="/login")
-    public String hello(){
-        return "login";
+    @RequestMapping(value="/index")
+    public ModelAndView index(){
+        User user = new User();
+        user.setName("tom");
+        user.setAge(10);
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("user", user);
+        mv.setViewName("/index.html");
+        return mv;
     }
 }
