f4d3f69e057da8e3e6b27886d14a217be5103292
hxp <15810665479@163.com>
2019-04-16 14:31:27 +0800

    提交

--- src/main/java/com/bling/dab/aspect/LogAspect.java
+++ src/main/java/com/bling/dab/aspect/LogAspect.java
@@ -1,20 +1,25 @@
 package com.bling.dab.aspect;
 
 import com.alibaba.fastjson.JSON;
-import com.bling.dab.common.util.DateUtil;
 import com.bling.dab.domain.Log;
 import com.bling.dab.service.LogService;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StopWatch;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
-import java.lang.annotation.Annotation;
+import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
 import java.util.Date;
 
 /**
@@ -46,37 +51,67 @@ public class LogAspect {
     @Around("log()")
     public Object around(ProceedingJoinPoint proceedingJoinPoint){
         Object[] args = proceedingJoinPoint.getArgs();
-        String methodName = proceedingJoinPoint.getSignature().getName();
-        String className = proceedingJoinPoint.getClass().getName();
+        Signature signature = proceedingJoinPoint.getSignature();
+        MethodSignature methodSignature = (MethodSignature)signature;
+        Method targetMethod = methodSignature.getMethod();
+        //方法名
+        String methodName = targetMethod.getName();
+        //类名
+        String className = methodSignature.getDeclaringType().getName();
         Object proceed = null;
         long millis = 0L;
+        Log log = new Log();
+        logger.info("调用"+methodName+"入参"+JSON.toJSONString(args));
+        sw.start("开始调用"+methodName);
+        com.bling.dab.common.annotation.Log annotation = targetMethod.getAnnotation(com.bling.dab.common.annotation.Log.class);
+        String action = annotation.action();
+        String description = annotation.description();
+        String modelName = annotation.modelName();
+        RequestAttributes ra = RequestContextHolder.getRequestAttributes();
+        ServletRequestAttributes sra = (ServletRequestAttributes) ra;
+        HttpServletRequest request = sra.getRequest();
+        log.setModelName(modelName);
+        log.setDescription(description);
+        log.setAction(action);
+        log.setClassName(className);
+        log.setCreateTime(new Date());
+        log.setMethodArgs(JSON.toJSONString(args));
+        log.setMethodName(methodName);
+        log.setUserId(1);
+        log.setIp(getIpAddress(request));
         try {
-            logger.info("调用"+methodName+"入参"+JSON.toJSONString(args));
-            sw.start("开始调用"+methodName);
             proceed = proceedingJoinPoint.proceed();
-            sw.stop();
-            millis = sw.getLastTaskTimeMillis();
-            Log log = new Log();
-            com.bling.dab.common.annotation.Log[] annotations = proceedingJoinPoint.getClass().getAnnotationsByType(com.bling.dab.common.annotation.Log.class);
-            String action = annotations[0].action();
-            String description = annotations[0].description();
-            String modelName = annotations[0].modelName();
-            log.setModelName(modelName);
-            log.setDescription(description);
-            log.setAction(action);
-            log.setClassName(className);
-            log.setCreateTime(new Date());
-            log.setMethodArgs(JSON.toJSONString(args));
             log.setSuccess("true");
-            log.setUserId(1);
-            log.setIp("");
-            log.setMessage("cg");
-            int i = logService.saveLog(log);
-            logger.info("调用"+methodName+"结果"+(i>0)+",返回值为"+JSON.toJSONString(proceed)+",耗时"+millis);
+            log.setMessage("执行成功");
         } catch (Throwable throwable) {
-            throwable.printStackTrace();
+            log.setSuccess("false");
+            log.setMessage("执行异常"+throwable.getMessage());
             logger.error("调用"+methodName+"异常",throwable);
         }
+        sw.stop();
+        millis = sw.getLastTaskTimeMillis();
+        logger.info("调用"+methodName+"结果返回值为"+JSON.toJSONString(proceed)+",耗时"+millis);
+        logService.saveLog(log);
         return proceed;
+    }
+
+    private  String getIpAddress(HttpServletRequest request) {
+        String ip = request.getHeader("x-forwarded-for");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        return ip+":"+request.getRemotePort();
     }
 }
